a0e88dd6913f8137297bff1bee36c7a5536f1a3d
Md Ashfaque Rahman <ashfaque.rahman.sifat@gmail.com>
2021-08-03 00:07:59 -0400

    Added new_random_generator.py

---
+++ new_random_generator.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+p = np.random.rand(2,3)
+
+print("Sifat's random number:")
+print(p)
--- random_number_generator.py
+++ random_number_generator.py
@@ -3,3 +3,4 @@ import numpy as np
 p = np.random.rand(2,3)
 
 print(p)
+print("This is end")
